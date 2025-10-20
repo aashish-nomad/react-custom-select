f9dc66400335b56d7fc4e6d16d9e3dd2d8fd7ba4
aashish-nomad <aashish.nomad@gmail.com>
2025-10-21 00:23:48 +0545

    Adds logic to select, clear and toggle the custom select

--- src/Select.tsx
+++ src/Select.tsx
@@ -14,15 +14,31 @@ type SelectProps = {
 
 export function Select({value, onChange, options}: SelectProps) {
   const [isOpen, setIsOpen] = useState(false)
+
+  function clearOptions() {
+    onChange(undefined)
+  }
+
+  function selectOption(option: SelectOption) {
+    onChange(option)
+  }
+
   return(
     <div onBlur={() => setIsOpen(false)} onClick={() => setIsOpen(val => !val)} className={styles.container} tabIndex={0}>
       <span className={styles.value}>{value?.label}</span>
-      <button className={styles['clear-btn']}>&times;</button>
+      <button className={styles['clear-btn']} onClick={(e) => {
+        e.stopPropagation()
+        clearOptions()
+      }}>&times;</button>
       <div className={styles.divider}></div>
       <div className={styles.caret}></div>
       <ul className={`${styles.options} ${isOpen ? styles.show : ''}`}>
         {options.map(option => {
-          return <li key={option.value} className={styles.option}>{option.label}</li>
+          return <li key={option.value} className={styles.option} onClick={(e) => {
+            e.stopPropagation()
+            selectOption(option)
+            setIsOpen(false)
+          }}>{option.label}</li>
         })}
       </ul>
     </div>
