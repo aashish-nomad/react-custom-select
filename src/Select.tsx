5d1734622d18a013b0708281b045efcbc0a0fef6
aashish-nomad <aashish.nomad@gmail.com>
2025-10-21 09:03:46 +0545

    Adds logic to a highlight and hover the options

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from './css/select.module.css'
 
 type SelectOption = {
   label: string
-  value: any
+  value: string | number
 }
 
 type SelectProps = {
@@ -14,13 +14,24 @@ type SelectProps = {
 
 export function Select({value, onChange, options}: SelectProps) {
   const [isOpen, setIsOpen] = useState(false)
+  const [higlightedIndex, setHiglightedIndex] = useState(0)
+
+  useEffect(() => {
+    if (isOpen) {
+      setHiglightedIndex(0)
+    }
+  }, [isOpen])
 
   function clearOptions() {
     onChange(undefined)
   }
 
   function selectOption(option: SelectOption) {
-    onChange(option)
+    if (option != value ) onChange(option)
+  }
+
+  function isOptionSelected(option: SelectOption) {
+    return option === value
   }
 
   return(
@@ -33,12 +44,23 @@ export function Select({value, onChange, options}: SelectProps) {
       <div className={styles.divider}></div>
       <div className={styles.caret}></div>
       <ul className={`${styles.options} ${isOpen ? styles.show : ''}`}>
-        {options.map(option => {
-          return <li key={option.value} className={styles.option} onClick={(e) => {
-            e.stopPropagation()
-            selectOption(option)
-            setIsOpen(false)
-          }}>{option.label}</li>
+        {options.map((option, index) => {
+          return (
+            <li
+              key={option.value}
+              className={`${styles.option} ${ isOptionSelected(option) ? styles.selected : ''} ${ (higlightedIndex == index) ? styles.highlighted : ''} `}
+              onClick={(e) => {
+                e.stopPropagation()
+                selectOption(option)
+                setIsOpen(false)
+              }}
+              onMouseEnter={() => {
+                setHiglightedIndex(index)
+              }}
+              >
+              {option.label}
+            </li>
+            )
         })}
       </ul>
     </div>
